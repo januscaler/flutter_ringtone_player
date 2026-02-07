7eb8647e6fcfe80e5efa0540368426684bb3d746
shivanshtalwar0 <shivanshtalwar0@gmail.com>
2026-02-08 03:30:02 +0530

    fix test

--- test/flutter_notifications_sound_test.dart
+++ test/flutter_notifications_sound_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
+import 'package:januscaler_flutter_ringtone_player/flutter_ringtone_player.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
